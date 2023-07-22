d62afc0952007a2a57b0d6660ffd1b520e1e8a8f
mattiapomelli <mattiapomelli@gmail.com>
2023-07-22 18:09:40 +0200

    Refactor attestation submission

--- src/pages/eas.tsx
+++ src/pages/eas.tsx
@@ -2,7 +2,6 @@ import {
   AttestationShareablePackageObject,
   EAS,
   SchemaEncoder,
-  createOffchainURL,
 } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 import { useEffect } from "react";
@@ -22,124 +21,54 @@ export default function EasPage() {
   const { data: signer } = useSigner();
 
   useEffect(() => {
-    if (!address || !signer) return;
+    if (!signer) return;
 
-    const connectEas = async () => {
-      // Initialize the sdk with the address of the EAS Schema contract address
-
-      // Gets a default provider (in production use something else like infura/alchemy)
-      const provider = ethers.providers.getDefaultProvider("sepolia");
-
-      // Connects an ethers style provider/signingProvider to perform read/write functions.
-      // MUST be a signer to do write operations!
-      eas.connect(signer);
-
-      console.log("Connected to EAS");
-
-      // const uid =
-      //   "0x10e4079bc6309830f6ea958adc9623b4cdb42d0e93d2ddc27f77cd17a8155863";
-      // const attestation = await eas.getAttestation(uid);
-
-      // console.log("Attestation: ", attestation);
-    };
-
-    connectEas();
-  }, [address, signer]);
+    // Connects an ethers style provider/signingProvider to perform read/write functions.
+    // MUST be a signer to do write operations!
+    eas.connect(signer);
+  }, [signer]);
 
   const createAttestation = async () => {
     if (!address || !signer) return;
 
-    eas.connect(signer);
-
     const offchain = await eas.getOffchain();
+    const provider = ethers.providers.getDefaultProvider("sepolia");
+    const wallet = new ethers.Wallet(privateKey, provider);
 
-    const schemaEncoder = new SchemaEncoder("bool confirm");
+    // Initialize SchemaEncoder with the schema string
+    const schemaEncoder = new SchemaEncoder(
+      "string snapshotProposalId,uint8 choice",
+    );
     const encodedData = schemaEncoder.encodeData([
-      { name: "confirm", type: "bool", value: true },
+      { name: "snapshotProposalId", value: "abcd", type: "string" },
+      { name: "choice", value: 1, type: "uint8" },
     ]);
 
-    const CUSTOM_SCHEMAS = {
-      MET_IRL_SCHEMA:
-        "0xc59265615401143689cbfe73046a922c975c99d97e4c248070435b1104b2dea7",
-      CONFIRM_SCHEMA:
-        "0x4eb603f49d68888d7f8b1fadd351b35a252f287ba465408ceb2b1e1e1efd90d5",
-    };
-    const signedOffchainAttestation = await offchain.signOffchainAttestation(
+    const offchainAttestation = await offchain.signOffchainAttestation(
       {
-        schema: CUSTOM_SCHEMAS.CONFIRM_SCHEMA,
-        recipient: ethers.constants.AddressZero,
-        refUID: data.id,
-        data: encoded,
-        time: dayjs().unix(),
-        revocable: true,
+        recipient: "0x0F45421E8DC47eF9edd8568a9D569b6fc7Aa7AC6",
+        // Unix timestamp of when attestation expires. (0 for no expiration)
         expirationTime: 0,
+        // Unix timestamp of current time
+        time: Math.floor(Date.now() / 1000),
+        revocable: false,
         version: 1,
         nonce: 0,
+        schema:
+          "0x32e5d6d35c252c5d52d72a8e989d460c78816386fa20478a0464fa46de775267",
+        refUID:
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+        data: encodedData,
       },
-      signer as unknown as TypedDataSigner, // Wagmi doesnt believe signer is typeddatasigner
+      wallet,
     );
 
     const pkg: AttestationShareablePackageObject = {
-      signer: address,
-      sig: signedOffchainAttestation,
+      signer: wallet.address,
+      sig: offchainAttestation,
     };
 
-    const result = await submitSignedAttestation(pkg);
-
-    // _________
-
-    // Initialize SchemaEncoder with the schema string
-    // const schemaEncoder = new SchemaEncoder(
-    //   "string snapshotProposalId,uint8 choice",
-    // );
-    // const encodedData = schemaEncoder.encodeData([
-    //   { name: "snapshotProposalId", value: "abcd", type: "string" },
-    //   { name: "choice", value: 1, type: "uint8" },
-    // ]);
-
-    // const wallet = ethers.Wallet.createRandom();
-    // const provider = ethers.providers.getDefaultProvider("sepolia");
-    // const wallet = new ethers.Wallet(privateKey, provider);
-
-    // console.log("Wallet: ", wallet.address);
-
-    // const offchainAttestation = await offchain.signOffchainAttestation(
-    //   {
-    //     recipient: "0x0F45421E8DC47eF9edd8568a9D569b6fc7Aa7AC6",
-    //     // Unix timestamp of when attestation expires. (0 for no expiration)
-    //     expirationTime: 0,
-    //     // Unix timestamp of current time
-    //     time: Math.floor(Date.now() / 1000),
-    //     revocable: false,
-    //     version: 1,
-    //     nonce: 0,
-    //     schema:
-    //       "0x32e5d6d35c252c5d52d72a8e989d460c78816386fa20478a0464fa46de775267",
-    //     refUID:
-    //       "0x0000000000000000000000000000000000000000000000000000000000000000",
-    //     data: encodedData,
-    //   },
-    //   wallet,
-    // );
-    // console.log("offchainAttestation", offchainAttestation);
-
-    // const url = createOffchainURL({
-    //   signer: wallet.address,
-    //   sig: offchainAttestation,
-    // });
-
-    // console.log("url", url);
-
-    // const pkg: AttestationShareablePackageObject = {
-    //   signer: address,
-    //   sig: offchainAttestation,
-    // };
-
-    // const result = await submitSignedAttestation(pkg);
-    // console.log("Result: ", result);
-
-    const attestation = await eas.getAttestation(offchainAttestation.uid);
-    console.log("Created attestation", attestation);
+    await submitSignedAttestation(pkg);
 
     console.log(
       "link",
